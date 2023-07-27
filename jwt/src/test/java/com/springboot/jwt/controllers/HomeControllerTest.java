9aad83d58d0963758dde8b60dbffe80051cae90c
Faizan Khokhar <muhammad.khokhar.97@gmail.com>
2023-07-27 17:52:09 +0500

    Test cases

---
+++ jwt/src/test/java/com/springboot/jwt/controllers/HomeControllerTest.java
@@ -0,0 +1,54 @@
+package com.springboot.jwt.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+
+import com.springboot.jwt.config.RsaKeyProperties;
+import com.springboot.jwt.config.SecurityConfig;
+import com.springboot.jwt.services.TokenService;
+
+@WebMvcTest({HomeController.class, AuthController.class})
+@Import({SecurityConfig.class, TokenService.class, RsaKeyProperties.class})
+public class HomeControllerTest {
+    
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    void rootWhenUnauthenticatedThen401() throws Exception {
+        this.mvc.perform(get("/"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void rootWhenAuthenticatedThenSaysHelloUser() throws Exception {
+        MvcResult result = this.mvc.perform(post("/token")
+                        .with(httpBasic("faizan", "password")))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String token = result.getResponse().getContentAsString();
+
+        this.mvc.perform(get("/")
+                        .header("Authorization", "Bearer " + token))
+                .andExpect(content().string("Hello, faizan"));
+    }
+
+    @Test
+    @WithMockUser
+    public void rootWithMockUserStatusIsOK() throws Exception {
+        this.mvc.perform(get("/")).andExpect(status().isOk());
+    }
+
+}
